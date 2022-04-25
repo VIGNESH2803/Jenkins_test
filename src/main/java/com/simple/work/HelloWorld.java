a7093d2fafecc7b5114b34f4177d60ecf77f2767
VIGNESH2803 <vignesh.s.2803@gmail.com>
2022-04-25 10:35:12 +0530

    jenkins test

---
+++ src/main/java/com/simple/work/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.simple.work;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World , Welcome to servlet using maven project");
+
+		
+	}
+
+}
